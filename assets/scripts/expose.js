a689612e7b5c66a2a2572ba3e3deeb0bc1ff1fc5
R3dbAbyVamp <ppeng@ucsd.edu>
2024-05-07 23:51:37 -0700

    comment out console log statements

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -11,16 +11,15 @@ selection.addEventListener('change', init);
 
 function init() {
   // TODO
-    console.log("hi");
-  
+    // console.log("hi");
     if (selection.selectedIndex == 1) {
-      console.log("air horn");
+      // console.log("air horn");
       image.src = "./assets/images/air-horn.svg"
     } else if (selection.selectedIndex == 2) {
-      console.log("car horn");
+      // console.log("car horn");
       image.src = "./assets/images/car-horn.svg"
     } else if (selection.selectedIndex == 3) {
-      console.log("party horn");
+      // console.log("party horn");
       image.src = "./assets/images/party-horn.svg"
     } else {
 
