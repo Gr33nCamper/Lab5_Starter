9600bf63f17a97d267797b4c908862733d74f6cb
R3dbAbyVamp <ppeng@ucsd.edu>
2024-05-07 23:49:58 -0700

    Add image changing functionality

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,28 @@
 // expose.js
 
-window.addEventListener('DOMContentLoaded', init);
+// window.addEventListener('DOMContentLoaded', init);
+
+var selection = document.getElementById('horn-select');
+var images = document.getElementsByTagName("img");
+var image = images[0];
+
+selection.addEventListener('change', init);
+
 
 function init() {
   // TODO
-}
+    console.log("hi");
+  
+    if (selection.selectedIndex == 1) {
+      console.log("air horn");
+      image.src = "./assets/images/air-horn.svg"
+    } else if (selection.selectedIndex == 2) {
+      console.log("car horn");
+      image.src = "./assets/images/car-horn.svg"
+    } else if (selection.selectedIndex == 3) {
+      console.log("party horn");
+      image.src = "./assets/images/party-horn.svg"
+    } else {
+
+    }
+  };
